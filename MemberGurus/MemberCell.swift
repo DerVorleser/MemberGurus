3e13ca0dee96194e1a7faa819ac4d222048d21e7
Nam <doanminhnam@gmail.com>
2017-02-14 18:40:08 +0700

    Shit got done

---
+++ MemberGurus/MemberCell.swift
@@ -0,0 +1,25 @@
+//
+//  MemberCell.swift
+//  MemberGurus
+//
+//  Created by Nam on 2/14/17.
+//  Copyright © 2017 Nam. All rights reserved.
+//
+
+import UIKit
+
+class MemberCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var platform: UILabel!
+    
+    func configureCell(member: Member) {
+        name.text = member.name
+        age.text = "$\(member.age)"
+        //platform.text = "$\(member.created)"
+        
+    }
+    
+}
